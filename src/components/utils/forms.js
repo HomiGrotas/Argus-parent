072181d94edd50c514107922aaed74c4731d45fd
homigrotas <shaninadav10@gmail.com>
2022-06-03 18:44:16 +0300

    show password on toggle

--- src/components/utils/forms.js
+++ src/components/utils/forms.js
@@ -1,12 +1,17 @@
+import { useState } from "react";
+
+
 const FormButton = (props) => {
+    const [showPassword, setShowPassword] = useState(false);
+
     return (
-        <tr>
+        <tr onMouseEnter={() => {setShowPassword(true)}} onMouseOut={() => {setShowPassword(false)}}>
             <td className="test">
-                <span className="glyphicon glyphicon-info-sign form-group"> </span>
+                <span className="glyphicon glyphicon-info-sign form-group"></span>
                 <span className="tooltip-text">{props.tip}</span>
             </td>
             <td>
-                <input type={props.type} className="form-control form-group" placeholder={props.placeholder} value={props.stateHook} onChange={(e) => props.setter(e.target.value)}/>
+                <input type={props.type === "password" && showPassword ? "text" : props.type} className="form-control form-group" placeholder={props.placeholder} value={props.stateHook} onChange={(e) => props.setter(e.target.value)}/>
             </td>
         </tr>
     );
